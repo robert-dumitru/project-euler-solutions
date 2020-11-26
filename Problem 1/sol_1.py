c04910d9f2bf32ee201821ac0b89c437501d6c7a
Robert Dumitru <72272382+robert-dumitru@users.noreply.github.com>
2020-11-25 17:23:51 -0800

    Update sol_1.py

--- Problem 1/sol_1.py
+++ Problem 1/sol_1.py
@@ -7,7 +7,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 def multiples(x):
     '''
-    Returns the sum of the multiples of 3 and 5 below x
+    Returns the sum of the multiples of 3 and 5 below x.
     '''
     sum = 0
     for i in range(1, x):
